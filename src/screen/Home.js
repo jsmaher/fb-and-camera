d6295af58310ed055d20ac911b44ab1a815c44ee
JS Rajput <jsmaherrajput.com>
2019-11-18 15:41:13 +0500

    fbook and camera react native

---
+++ src/screen/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Camera from './../Coponent/Camera'
+export default class Home extends React.Component {
+    render(){
+        return(
+            <Camera />
+        )
+    }
+}
